c3aaf6a1163ec42d4100ba77fb11232182b8ddb0
somed00d <41293874+somed00d@users.noreply.github.com>
2020-05-06 18:22:46 -0400

    Yikes

--- plugins/notifications.js
+++ plugins/notifications.js
@@ -2,7 +2,4 @@ import Vue from 'vue';
 import Notifications from 'vue-notification';
 import velocity from 'velocity-animate';
 
-// or for SSR:
-// import Notifications from 'vue-notification/dist/ssr.js'
-
 Vue.use(Notifications, {velocity});
