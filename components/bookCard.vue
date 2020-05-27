c92027703638c35550c12cbb8e99243cbd4be731
somed00d <41293874+somed00d@users.noreply.github.com>
2020-05-27 16:27:10 -0400

    Book list prelim

---
+++ components/bookCard.vue
@@ -0,0 +1,93 @@
+<template>
+	<div class="flex flex-auto justify-center">
+		<div class="relative h-64 pt-5">
+			<div class="bookBox relative bg-white rounded shadow-lg w-56 z-10">
+				<!-- Static parent -->
+
+				<div
+					style="background-color: #3a656a;"
+					class="bookDate absolute text-white text-xs font-extrabold top-0 right-0 p-1 rounded"
+				>
+					{{ date }}
+				</div>
+				<div class="static flex flex-col">
+					<div class="inline-block text-sm font-serif font-bold px-2 mt-4">
+						{{ title }}
+					</div>
+					<div style="color: #ff5e70;" class="inline-block text-center p-1">
+						{{ authors }}
+					</div>
+					<div
+						class="bookDesc hideScroll inline-block text-xs overflow-auto rounded px-2 italic"
+					>
+						{{ desc }}
+					</div>
+				</div>
+			</div>
+
+			<img
+				:src="imageUrl"
+				:alt="imageAlt"
+				class="absolute top-0 left-0 h-48 z-0 animate__animated animate__fadeIn"
+			/>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'BookCard',
+	props: {
+		title: {
+			type: String,
+			default: 'Title'
+		},
+		date: {
+			type: Number,
+			default: 2016
+		},
+		authors: {
+			type: String,
+			default: 'Authors'
+		},
+		desc: {
+			type: String,
+			default: 'Description'
+		},
+		imageUrl: {
+			type: String,
+			default: '#'
+		},
+		imageAlt: {
+			type: String,
+			default: 'Image'
+		}
+	}
+};
+</script>
+
+<style lang="scss">
+.bookBox {
+	margin-left: 75px;
+	max-height: 16rem;
+}
+.bookDesc {
+	height: 7rem;
+	max-height: 8rem;
+	margin-bottom: 0.5rem;
+}
+.bookDate {
+	margin-top: -15px;
+	margin-right: 15px;
+}
+
+/* Hide scrollbar for Chrome, Safari and Opera */
+.hideScroll::-webkit-scrollbar {
+	display: none;
+}
+
+/* Hide scrollbar for IE and Edge */
+.hideScroll {
+	-ms-overflow-style: none;
+}
+</style>
--- pages/booklist.vue
+++ pages/booklist.vue
@@ -1,8 +1,8 @@
 <template>
 	<div>
 		<div
-			style="color: #3a656a;"
-			class="rounded bg-white overflow-hidden shadow-md m-4"
+			style="color: #3a656a; background-color: #eef3ed;"
+			class="rounded bg-white overflow-hidden shadow-md m-4 h-full"
 		>
 			<div
 				style="font-family: 'Patua One', cursive;"
@@ -10,12 +10,37 @@
 			>
 				BOOK LIST
 			</div>
+			<div class="flex flex-auto flex-wrap my-5">
+				<div v-for="book in bookList" :key="book.id">
+					<BookCard
+						class="m-6"
+						:title="book.title"
+						:date="book.datePublished"
+						:authors="book.authors"
+						:desc="book.desc"
+						:image-url="book.imageUrl"
+						:image-alt="book.imageAlt"
+					/>
+				</div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+import BookCard from '~/components/bookCard';
+import myBookList from '~/assets/booklist.json';
+export default {
+	name: 'BookList',
+	components: {
+		BookCard
+	},
+	data() {
+		return {
+			bookList: myBookList.books
+		};
+	}
+};
 </script>
 
 <style></style>
