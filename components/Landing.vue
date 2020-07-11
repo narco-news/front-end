a70923b09e7d4c639b35a39193df802a50005ead
somed00d <41293874+somed00d@users.noreply.github.com>
2020-07-11 01:46:36 -0400

    Removed definition boxes

--- components/Landing.vue
+++ components/Landing.vue
@@ -1,8 +1,10 @@
 <template>
-  <div>
-    <div class="relative">
-      <div class="static flex flex-col md:flex-row justify-center items-center image-land">
-        <div class="static bg-white z-10 h-32 w-56 rounded-md m-3 md:m-4 md:mx-10 p-4">
+	<div>
+		<div class="relative">
+			<div
+				class="static flex flex-col md:flex-row justify-center items-center image-land"
+			>
+				<!-- <div class="static bg-white z-10 h-32 w-56 rounded-md m-3 md:m-4 md:mx-10 p-4">
           <div class="grid grid-cols-4">
             <div
               class="col-span-2 text-3xl text-center font-serif font-extrabold"
@@ -35,15 +37,15 @@
           </div>
           <div class="text-md text-center mt-1">Newly received or noteworthy information</div>
           <div class="text-xs text-center italic">"we have some news for you"</div>
-        </div>
-        <img
-          src="~/assets/images/opium.jpg"
-          alt="Poppy plants"
-          class="object-cover w-full absolute z-0 image-land"
-        />
-      </div>
-    </div>
-  </div>
+        </div> -->
+				<img
+					src="~/assets/images/opium.jpg"
+					alt="Poppy plants"
+					class="object-cover w-full absolute z-0 image-land"
+				/>
+			</div>
+		</div>
+	</div>
 </template>
 <script>
 export default {};
@@ -51,27 +53,27 @@ export default {};
 
 <style lang="scss">
 .image-land {
-  height: 20rem;
+	height: 20rem;
 }
 
 .definition {
-  p {
-    font-family: 'Baloo Thambi 2', cursive;
-  }
+	p {
+		font-family: 'Baloo Thambi 2', cursive;
+	}
 }
 .word {
-  h2 {
-    font-family: 'Patua One', display;
-  }
+	h2 {
+		font-family: 'Patua One', display;
+	}
 }
 .exp {
-  font-family: 'Montserrat', sans-serif;
-  font-style: italic;
+	font-family: 'Montserrat', sans-serif;
+	font-style: italic;
 }
 .gram {
-  color: #ff5e70;
+	color: #ff5e70;
 }
 .opium {
-  background-image: ('images/opium.jpg');
+	background-image: ('images/opium.jpg');
 }
 </style>
