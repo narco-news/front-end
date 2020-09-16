a997d5ce67330e874205e484cb2f6f5d3daaf2da
somed00d <41293874+somed00d@users.noreply.github.com>
2020-09-16 12:27:05 -0400

    Categories translated

--- components/TagsList.vue
+++ components/TagsList.vue
@@ -1,55 +1,89 @@
 <template>
 	<div>
 		<h1 class="desc text-xl font-bold ml-12 my-4">
-			Categories
+			{{ $t('message.tags') }}
 		</h1>
 		<div
 			class="cats grid grid-flow-col grid-cols-2 grid-rows-4 text-lg md:text-xl bg-white shadow-inner rounded-md mx-8"
 		>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/united-states"
-					>United States</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/united-states">{{
+					$t('message.us')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/mexican-dto">Mexico</n-link>
+				<n-link class="text-md md:text-md" to="/tag/mexican-dto">{{
+					$t('message.mx')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/central-america"
-					>Central America</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/central-america">{{
+					$t('message.ca')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/south-america"
-					>South America</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/south-america">{{
+					$t('message.sa')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/southeast-asia"
-					>Southeast Asia</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/southeast-asia">{{
+					$t('message.sea')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/film-and-television"
-					>Film & Television</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/film-and-television">{{
+					$t('message.ft')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/announcements"
-					>Announcements</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/announcements">{{
+					$t('message.an')
+				}}</n-link>
 			</div>
 			<div class="px-2 lg:px-4">
-				<n-link class="text-md md:text-md" to="/tag/development"
-					>Development</n-link
-				>
+				<n-link class="text-md md:text-md" to="/tag/development">{{
+					$t('message.dev')
+				}}</n-link>
 			</div>
 		</div>
 	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	name: 'TagsList',
+	i18n: {
+		messages: {
+			en: {
+				message: {
+					tags: 'Categories',
+					us: 'United States',
+					mx: 'Mexico',
+					ca: 'Central America',
+					sa: 'South America',
+					sea: 'Southeast Asia',
+					ft: 'Film & Television',
+					an: 'Announcements',
+					dev: 'Development'
+				}
+			},
+			es: {
+				message: {
+					tags: 'Categorías',
+					us: 'Estados Unidos',
+					mx: 'México',
+					ca: 'América Central',
+					sa: 'América del Sur',
+					sea: 'El sudeste de Asia',
+					ft: 'Cine y Televisión',
+					an: 'Anuncios',
+					dev: 'Desarrollo de sitios web'
+				}
+			}
+		}
+	}
+};
 </script>
 
 <style lang="scss" scoped>
