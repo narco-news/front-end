db67babd11e1856458a654fb09e51c8b890c8be7
somed00d <41293874+somed00d@users.noreply.github.com>
2020-09-16 22:38:20 -0400

    Add new Guanajuato map

--- pages/guanajuato-map.vue
+++ pages/guanajuato-map.vue
@@ -1,5 +1,10 @@
 <template>
-	<div id="map" class="mt-4 sm:mt-8 sm:mx-5 sm:mb-5"></div>
+	<div class="m-4">
+		<div
+			id="map"
+			class="mt-4 sm:mt-8 sm:mx-5 sm:mb-5 rounded-md shadow-md"
+		></div>
+	</div>
 </template>
 
 <script>
@@ -7,17 +12,15 @@ export default {
 	name: 'GuanajuatoMap',
 	data() {
 		return {
-			selectedMarker: null,
-			markers: [],
-			map: null
+			map: null,
+			markers: []
 		};
 	},
-	// computed: {
-	// 	incidents() {
-	// 		return this.$store.state.incidents;
-	// 	}
-	// },
-	// https://vuejs.org/v2/api/#activated
+	computed: {
+		incidents() {
+			return this.$store.state.incidents;
+		}
+	},
 	activated() {
 		if (!this.map || !this.map.loaded()) return;
 		this.toggleMarker();
@@ -33,97 +36,82 @@ export default {
 	},
 	mounted() {
 		const mapboxgl = require('mapbox-gl');
-		const geojson = {
-			type: 'FeatureCollection',
-			features: [
-				{
-					type: 'Feature',
-					properties: {
-						'marker-color': '#ee1919',
-						'marker-size': 'medium',
-						'marker-symbol': 'police'
-					},
-					geometry: {
-						type: 'Point',
-						coordinates: [-101.18408203124999, 20.679044609010408]
-					}
-				},
-				{
-					type: 'Feature',
-					properties: {
-						'marker-color': '#2333e7',
-						'marker-size': 'medium',
-						'marker-symbol': ''
-					},
-					geometry: {
-						type: 'Point',
-						coordinates: [-101.5521240234375, 21.12549763660628]
-					}
-				},
-				{
-					type: 'Feature',
-					properties: {
-						'marker-color': '#12dc4d',
-						'marker-size': 'medium',
-						'marker-symbol': ''
-					},
-					geometry: {
-						type: 'Point',
-						coordinates: [-100.799560546875, 20.5967942442688]
-					}
-				}
-			]
-		};
-		const bounds = [
-			[-101.918758, 20.191622], // Southwest coordinates
-			[-100.090473, 21.591358] // Northeast coordinates
-		];
-
+		// const bounds = [
+		// 	[-101.918758, 20.191622], // Southwest coordinates
+		// 	[-100.090473, 21.591358] // Northeast coordinates
+		// ];
 		this.map = new mapboxgl.Map({
 			accessToken: process.env.accessToken,
 			container: 'map',
 			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [-101.266667, 21.016667],
-			zoom: 0,
+			center: [-100.99, 20.51],
+			zoom: 8,
 			pitch: 0,
-			maxZoom: 17,
-			maxBounds: bounds
+			minZoom: 8,
+			maxZoom: 17
+			// maxBounds: bounds
 		});
-
-		geojson.features.forEach(marker => {
-			// create a HTML element for each feature
-			const el = document.createElement('div');
-			el.className = 'marker';
-
-			// make a marker for each feature and add to the map
-			new mapboxgl.Marker(el)
-				.setLngLat(marker.geometry.coordinates)
+		this.incidents.geojson.features.forEach(marker => {
+			const LngLat = [
+				marker.geometry.coordinates[0],
+				marker.geometry.coordinates[1]
+			];
+			let popup = `<h2 class="text-xl mb-2 mt-2">${marker.properties.title}</h2>`;
+			popup += `<p class="text-md font-bold">${marker.properties.muni}</p>`;
+			popup += `<p class="my-1 text-sm">Date: <span class="font-bold">${marker.properties.date}</span></p>`;
+			popup += `<p class="text-sm p-1">${marker.properties.desc}</p>`;
+			popup += `<p class="font-mono text-xs text-green-500 px-1 mt-1">${LngLat[0]}</p>`;
+			popup += `<p class="font-mono text-xs text-green-500 px-1">${LngLat[1]}</p>`;
+			popup += `<p class="text-sm p-2 font-bold">Sources:</p>`;
+			popup += `<div class="px-2">
+			<a href="${marker.properties.links}" class="underline text-blue-500 text-sm" target="_blank">
+			<svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13.828 10.172a4 4 0 00-5.656 0l-4 4a4 4 0 105.656 5.656l1.102-1.101m-.758-4.899a4 4 0 005.656 0l4-4a4 4 0 00-5.656-5.656l-1.1 1.1"></path></svg>
+			</a>
+			</div>`;
+			const popupElement = new mapboxgl.Popup({
+				offset: -10,
+				anchor: 'bottom'
+			}).setHTML(popup);
+			popupElement.on('close', () => {
+				if (this.selectedMarker) {
+					this.selectedMarker.classList.remove('hidden');
+				}
+			});
+			new mapboxgl.Marker({color: marker.properties.color})
+				.setLngLat(LngLat)
+				.setPopup(popupElement)
 				.addTo(this.map);
 		});
-
 		this.map.addControl(new mapboxgl.NavigationControl());
-		this.map.addControl(new mapboxgl.FullscreenControl());
-
+		this.map.addControl(new mapboxgl.FullscreenControl(), 'top-left');
+		this.scrollToBottom();
 		this.toggleMarker();
 	},
 	methods: {
-		toggleMarker() {
-			const street = this.$route.query.street;
-			if (!street) return;
-			const marker = this.markers.find(marker => marker.street === street);
-			if (!marker) return;
-			this.map.jumpTo({
-				center: marker.getLngLat()
+		toggleMarker() {},
+		scrollToBottom() {
+			if (!window) return;
+			window.scrollTo({
+				top: 40,
+				left: 0,
+				behavior: 'smooth'
 			});
-			marker.togglePopup();
 		}
+	},
+	head() {
+		return {
+			link: [
+				{
+					rel: 'stylesheet',
+					href: 'https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css'
+				}
+			]
+		};
 	}
 };
 </script>
 
-<style lang="postcss">
-@import url('https://api.mapbox.com/mapbox-gl-js/v1.8.1/mapbox-gl.css');
-
+<style lang="scss">
 .mapboxgl-popup {
 	will-change: auto;
 	min-width: 200px;
@@ -131,54 +119,56 @@ export default {
 }
 
 .mapboxgl-popup-content {
-	font-family: europa, sans-serif;
-	@apply text-white bg-black rounded-none pt-16 px-6 pb-12 leading-snug text-lg;
+	@apply text-lg;
+	font-family: 'Source Sans Pro', sans;
+	background-color: #262626;
+	color: #f2f2f2;
+	padding: 1em 1em;
+	h2 {
+		font-family: 'EB Garamond', sans-serif;
+		font-weight: 700;
+	}
 }
 
 .mapboxgl-popup-close-button {
-	@apply text-3xl mt-2 mr-4;
+	@apply text-3xl;
+	@apply mt-2;
+	@apply mr-4;
 }
 
 .mapboxgl-popup-tip {
+	margin-top: -2px;
 	border: 2rem solid transparent;
 }
 
 .mapboxgl-popup-anchor-top .mapboxgl-popup-tip,
 .mapboxgl-popup-anchor-top-left .mapboxgl-popup-tip,
 .mapboxgl-popup-anchor-top-right .mapboxgl-popup-tip {
-	border-bottom-color: black;
+	border-bottom-color: #262626;
 }
 
 .mapboxgl-popup-anchor-bottom .mapboxgl-popup-tip,
 .mapboxgl-popup-anchor-bottom-left .mapboxgl-popup-tip,
 .mapboxgl-popup-anchor-bottom-right .mapboxgl-popup-tip {
-	border-top-color: black;
+	border-top-color: #262626;
 }
 
 .mapboxgl-popup-anchor-left .mapboxgl-popup-tip {
-	border-right-color: black;
+	border-right-color: #262626;
 }
 
 .mapboxgl-popup-anchor-right .mapboxgl-popup-tip {
-	border-left-color: black;
+	border-left-color: #262626;
 }
 
 #map {
 	min-height: 75vh;
-}
-
-@screen md {
-	#map {
+	@media (max-width: 768px) {
 		min-height: 85vh;
 	}
 }
 
 .marker {
-	background-image: url('/assets/icons/map-pin-red.svg');
-	background-size: cover;
-	width: 50px;
-	height: 50px;
-	border-radius: 50%;
 	cursor: pointer;
 }
 </style>
--- store/incidents.js
+++ store/incidents.js
@@ -1,17 +1,57 @@
 export const state = () => ({
-	list: {
+	geojson: {
 		type: 'FeatureCollection',
 		features: [
 			{
 				type: 'Feature',
-				properties: {
-					'marker-color': '#ee1919',
-					'marker-size': 'medium',
-					'marker-symbol': 'police'
-				},
 				geometry: {
 					type: 'Point',
-					coordinates: [-101.18408203124999, 20.679044609010408]
+					coordinates: [-100.83970919251442, 20.54082290446217]
+				},
+				properties: {
+					color: '#F26457',
+					title: 'Female with cartulina',
+					date: '15/09/2020',
+					muni: 'San Juanico, Celaya',
+					desc: 'Punisher logo, CSRL and triangle, similar handwriting',
+					links: [
+						'https://celayanews.com/seguridad/encuentran-cuerpo-decapitado-en-san-juanico-celaya-durante-la-madrugada/'
+					]
+				}
+			},
+			{
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: [-100.99357008934021, 20.17595718444703]
+				},
+				properties: {
+					color: '#F26457',
+					title: 'Male with cartulina',
+					date: '26/08/2020',
+					muni: 'Santiago Maravatío',
+					desc: '',
+					links: [
+						'https://puntocero.news/2020/08/26/localizan-a-ejecutado-en-santiago-maravatio/'
+					]
+				}
+			},
+			{
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: [-100.56145548820494, 20.512670653111872]
+				},
+				properties: {
+					color: '#F26457',
+					title: 'Male with cartulina',
+					date: '24/08/2020',
+					muni: 'Marroquín',
+					desc: '',
+					links: [
+						'https://www.am.com.mx/guanajuato/noticias/Desmembrado-y-dentro-de-una-maleta-encuentran-a-hombre-en-Celaya-20200825-0024.html',
+						'https://www.facebook.com/permalink.php?story_fbid=1451018655104502&id=425140334359011'
+					]
 				}
 			}
 		]
